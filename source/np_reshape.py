6af1070266c1dcb4a4c5b2f07a4b7c528f0d77c5
kn.fang <fangshenghui@gmail.com>
2025-10-31 14:57:03 +0800

    feat:note

--- source/np_reshape.py
+++ source/np_reshape.py
@@ -4,12 +4,13 @@ import numpy as np
 rng = np.random.default_rng(seed=42)
 arr = rng.integers(1, 10, (3, 4))
 
-print("原始矩阵:", arr)
+print("原始矩阵:\n", arr)
 
 # 展平
-print("改变原矩阵 展平:", arr.ravel())  
-print("不改变原矩阵 展平:", arr.flatten())  
+print("改变原矩阵 展平:\n", arr.ravel())  
+print("不改变原矩阵 展平:\n", arr.flatten())  
 
 #### 扩展 1 个维度，需要（必须）指定维度
 # 其实就是多嵌套了一下
-print("扩展 1 个维度:", np.expand_dims(arr, 1))
+print("扩展 之前:\n", arr)
+print("扩展 1 个维度:\n", np.expand_dims(arr, 1))
